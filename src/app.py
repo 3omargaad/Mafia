380bd2e63b14d7db18d0ab61ff81d51a09392f5d
3omargaad <3omargaad@gmail.com>
2025-08-21 14:02:15 +0100

    Button Basic Click

--- src/app.py
+++ src/app.py
@@ -1,15 +1,19 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 from pathlib import Path
 
 class guiApp(App):
     def build(self):
         self.icon = str(Path("assets\\images\\mafia_logo.ico"))
 
-class guiWidget(Widget):
-    pass
+class guiWidget(GridLayout):
+    def on_play(self):
+        print("Welcome to Mafia! I am your host ChadGPT.")
 
 def setup():
     app = guiApp() 
     app.run()
     app.build()
+
+setup()
