5f24c21fcaba5bf259d944efe076cf2c356e7cda
3omargaad <3omargaad@gmail.com>
2025-08-21 14:37:32 +0100

    Added Font

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
 from pathlib import Path
 
 class guiApp(App):
@@ -8,8 +9,13 @@ class guiApp(App):
         self.icon = str(Path("assets\\images\\mafia_logo.ico"))
 
 class guiWidget(GridLayout):
+    outputText = StringProperty("Welcome to Mafia! I am your host ChadGPT.")
+    count = 0
+
     def on_play(self):
+        self.count += 1
         print("Welcome to Mafia! I am your host ChadGPT.")
+        self.outputText = str(self.count)
 
 def setup():
     app = guiApp() 
