b61e719e780618998ce2d9224905b8e604ca38b7
3omargaad <3omargaad@gmail.com>
2025-08-21 22:09:54 +0100

    Added test folder

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -4,37 +4,33 @@ import game_logic
 
 
 if __name__ == "__main__": 
-    game_logic.plrObject = game_logic.player.Player(name="ali", role="Civilian", team="Good", isAlive=True, audioFile=None) #nameAudio)
+    game_logic.plrObject = game_logic.player.Player(name="A", role="Civilian", team="Good", isAlive=True, audioFile=None) #nameAudio)
     game_logic.players.append(game_logic.plrObject)
     game_logic.livingPlayers.append(game_logic.plrObject)
 
-    game_logic.plrObject = game_logic.player.Player(name="belal", role="Doctor", team="Good", isAlive=True, audioFile=None) #nameAudio)
+    game_logic.plrObject = game_logic.player.Player(name="B", role="Doctor", team="Good", isAlive=True, audioFile=None) #nameAudio)
     game_logic.players.append(game_logic.plrObject)
     game_logic.livingPlayers.append(game_logic.plrObject)
 
-    game_logic.plrObject = game_logic.player.Player(name="omar", role="Detective", team="Good", isAlive=True, audioFile=None) #nameAudio)
+    game_logic.plrObject = game_logic.player.Player(name="C", role="Detective", team="Good", isAlive=True, audioFile=None) #nameAudio)
     game_logic.players.append(game_logic.plrObject)
     game_logic.livingPlayers.append(game_logic.plrObject)
 
-    game_logic.plrObject = game_logic.player.Player(name="hamza", role="Mafia", team="Bad", isAlive=True, audioFile=None) #nameAudio)
+    game_logic.plrObject = game_logic.player.Player(name="D", role="Mafia", team="Bad", isAlive=True, audioFile=None) #nameAudio)
     game_logic.players.append(game_logic.plrObject)
     game_logic.livingPlayers.append(game_logic.plrObject)
 
     game_logic.clear()
     while True: # Main Game Loop
-        game_logic.vote()
-        game_logic.execution()
-        if game_logic.hasGameEnded() == True:
-            break
-        game_logic.night()        
+        game_logic.night()
         game_logic.announcement()
         if game_logic.hasGameEnded() == True:
             break
         game_logic.day()
-        
-        
+        game_logic.vote()
+        game_logic.execution()
         if game_logic.hasGameEnded() == True:
             break
-
-    print("Game ended")
+        
     game_logic.endGame()
+
