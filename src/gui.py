c44275257e3e52f170bb292641ba5d842c750413
3omargaad <3omargaad@gmail.com>
2025-10-15 21:27:58 +0100

    Detects Majority Game Setup Inputs

--- src/gui.py
+++ src/gui.py
@@ -32,6 +32,8 @@ from kivy.lang import Builder
 # dedicated to managing multiple screens for your application.
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+import game_setup
+
 Builder.load_file("login.kv")
 Builder.load_file("account.kv")
 Builder.load_file("setup.kv")
@@ -92,30 +94,76 @@ class ScreenThree(Screen, FloatLayout, GridLayout):
 
     include_doc_val = BooleanProperty(False)
     include_det_val = BooleanProperty(False)
+    anonymous_voting_val = BooleanProperty(False)
+    allow_skip_val = BooleanProperty(False)
+    execute_if_tie_val = BooleanProperty(False)
+    host_name_val = StringProperty("")
+    host_accent_val = StringProperty("")
+
 
     def toggle(self):
         pass
 
-    def on_doc_swtich_active(self, widget):
-        print("Include Doctor? " + str(widget.active))
-        global include_doc_val
-        include_doc_val = widget.active
-    
-    def on_det_swtich_active(self, widget):
-        print("Include Detective? " + str(widget.active))
-        global include_det_val
-        include_det_val = widget.active
-    
     def on_plr_slider_value(self, widget):
         self.plr_num_val = str(int(widget.value))
+        game_setup.plr_num = int(widget.value)
           
     def on_maf_slider_value(self, widget):
         self.maf_num_val = str(int(widget.value))
+        game_setup.maf_num = int(widget.value)
         
     def on_time_slider_value(self, widget):
         self.discussion_time_val = str(int(widget.value))
+        game_setup.discussion_time = int(widget.value)
+
+    def on_doc_swtich_active(self, widget):
+        print("Include Doctor? " + str(widget.active))
+        self.include_doc_val = widget.active
+        game_setup.include_doc = self.include_doc_val
+    
+    def on_det_swtich_active(self, widget):
+        print("Include Detective? " + str(widget.active))
+        self.include_det_val = widget.active
+        game_setup.include_det = self.include_det_val
+
+    def on_anonymous_swtich_active(self, widget):
+        print("Anonymous Voting? " + str(widget.active))
+        self.anonymous_voting_val = widget.active
+        game_setup.anonymous_voting = self.anonymous_voting_val
+        
+    def on_skip_swtich_active(self, widget):
+        print("Allow Skip? " + str(widget.active))
+        self.allow_skip_val = widget.active
+        game_setup.allow_skip = self.allow_skip_val
+    
+    def on_execute_swtich_active(self, widget):
+        print("Execute If TIe? " + str(widget.active))
+        self.execute_if_tie_val = widget.active
+        game_setup.execute_if_tie = self.execute_if_tie_val
+    
+    def on_host_name_validate(self, widget):
+        print("Host Name: " + str(widget.value))
+        self.host_name_val = widget.active
+        game_setup.host_name = self.host_name_val
+
+    def on_play(self):
+        print("Player Number:", game_setup.plr_num)
+        print("Mafia Number:", game_setup.maf_num)
+        print("Discussion Time:", game_setup.discussion_time)
+        print("Include Doctor?", game_setup.include_doc)
+        print("Include Detective?", game_setup.include_det)
+        print("Anonymous Voting?", game_setup.anonymous_voting)
+        print("Allow Skip?", game_setup.allow_skip)
+        print("Execute If Tie?", game_setup.execute_if_tie)
+        print("Host Name:", game_setup.host_name)
+        print("Host Accent:", game_setup.host_accent)
 
 class ScreenFour(Screen):
+    rye_font = get_path("assets", "fonts", "Rye-Regular.ttf")
+    roboto_font = get_path("assets", "fonts", "RobotoSlab-Medium.ttf")
+    press_font = get_path("assets", "fonts", "PressStart2P-Regular.ttf")
+
+class ScreenFive(Screen):
     pass
  
 # The ScreenManager controls moving between screens
@@ -127,6 +175,7 @@ screen_manager.add_widget(ScreenOne(name ="screen_one"))
 screen_manager.add_widget(ScreenTwo(name ="screen_two"))
 screen_manager.add_widget(ScreenThree(name ="screen_three"))
 screen_manager.add_widget(ScreenFour(name ="screen_four"))
+screen_manager.add_widget(ScreenFour(name ="screen_five"))
 
 # Create the App class
 class MafiaApp(App):
