d7a86295115483b899c18659ca481c7a8446418b
3omargaad <3omargaad@gmail.com>
2026-03-24 07:58:37 +0000

    Build v0.3 (Temporarily removed audio)

--- src/audio.py
+++ src/audio.py
@@ -1,7 +1,8 @@
-from pydub import AudioSegment
-from pydub.playback import play
+# from pydub import AudioSegment
+# from pydub.playback import play
 
 
 def play_audio(audioFile):
-    file = AudioSegment.from_file(file=audioFile, format="wav")
-    play(file)
+    # file = AudioSegment.from_file(file=audioFile, format="wav")
+    # play(file)
+    pass
