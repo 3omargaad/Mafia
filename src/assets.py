676cc2e16eb5b5f9fa70b6e672f85d55be6860a1
3omargaad <3omargaad@gmail.com>
2026-04-18 17:56:40 +0100

    Added resource_find to get audio

--- src/assets.py
+++ src/assets.py
@@ -1,38 +1,39 @@
-from files import get_path
-
-
-def get_sound(name):
-    return get_path("assets", "audio", "preset", "uk", name)
+from kivy.resources import resource_find
+# from files import get_path
+# 
+# 
+# def get_sound(name):
+#     return get_path("assets", "audio", "preset", "uk", name)
 
 
 # RYE = get_path("assets", "fonts", "RobotoSlab-Medium.ttf")
 # ROBOTO = get_path("assets", "fonts", "RobotoSlab-Medium.ttf")
 
-WIN_IMG = get_path("assets", "images", "win_img.png")
-LOSE_IMG = get_path("assets", "images", "lose_img.png")
+WIN_IMG = resource_find("win_img.png")  # get_path("assets", "images", "win_img.png")
+LOSE_IMG = resource_find("lose_img.png")  # get_path("assets", "images", "lose_img.png")
 
-UI_CLICK = get_path("assets", "audio", "preset", "common", "ui_click.ogg")
-UI_ENABLE = get_path("assets", "audio", "preset", "common", "ui_enable.ogg")
-UI_DISABLE = get_path("assets", "audio", "preset", "common", "ui_disable.ogg")
-UI_POP = get_path("assets", "audio", "preset", "common", "ui_pop.ogg")
-WIN = get_path("assets", "audio", "preset", "common", "win.ogg")
-LOSE = get_path("assets", "audio", "preset", "common", "lose.ogg")
+UI_CLICK = resource_find("ui_click.ogg")  # get_path("assets", "audio", "preset", "common", "ui_click.ogg")
+UI_ENABLE = resource_find("ui_enable.ogg")  # get_path("assets", "audio", "preset", "common", "ui_enable.ogg")
+UI_DISABLE = resource_find("ui_disable.ogg")  # get_path("assets", "audio", "preset", "common", "ui_disable.ogg")
+UI_POP = resource_find("ui_pop.ogg")  # get_path("assets", "audio", "preset", "common", "ui_pop.ogg")
+WIN = resource_find("win.ogg")  # get_path("assets", "audio", "preset", "common", "win.ogg")
+LOSE = resource_find("lose.ogg")  # get_path("assets", "audio", "preset", "common", "lose.ogg")
 
-WELCOME = get_sound("welcome.ogg")
-INTRO = get_sound("intro.ogg")
-NIGHT = get_sound("night.ogg")
-MAFIA = get_sound("mafia.ogg")
-MAFIA_SLEEP = get_sound("mafia_sleep.ogg")
-DOCTOR = get_sound("doctor.ogg")
-DOCTOR_SLEEP = get_sound("doctor_sleep.ogg")
-DETECTIVE = get_sound("detective.ogg")
-DETECTIVE_SLEEP = get_sound("detective_sleep.ogg")
-MORNING = get_sound("morning.ogg")
-FORTUNATELY = get_sound("fortunately.ogg")
-UNFORTUNATELY = get_sound("unfortunately.ogg")
-DISCUSS = get_sound("discuss.ogg")
-VOTE = get_sound("vote.ogg")
-REVEAL = get_sound("reveal.ogg")
-EXECUTION = get_sound("execution.ogg")
-GAME_OVER = get_sound("game_over.ogg")
-GAME_CONTINUES = get_sound("game_continues.ogg")
+WELCOME = resource_find("welcome.ogg")
+INTRO = resource_find("intro.ogg")
+NIGHT = resource_find("night.ogg")
+MAFIA = resource_find("mafia.ogg")
+MAFIA_SLEEP = resource_find("mafia_sleep.ogg")
+DOCTOR = resource_find("doctor.ogg")
+DOCTOR_SLEEP = resource_find("doctor_sleep.ogg")
+DETECTIVE = resource_find("detective.ogg")
+DETECTIVE_SLEEP = resource_find("detective_sleep.ogg")
+MORNING = resource_find("morning.ogg")
+FORTUNATELY = resource_find("fortunately.ogg")
+UNFORTUNATELY = resource_find("unfortunately.ogg")
+DISCUSS = resource_find("discuss.ogg")
+VOTE = resource_find("vote.ogg")
+REVEAL = resource_find("reveal.ogg")
+EXECUTION = resource_find("execution.ogg")
+GAME_OVER = resource_find("game_over.ogg")
+GAME_CONTINUES = resource_find("game_continues.ogg")
