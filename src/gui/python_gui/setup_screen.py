18b5859c294abafe750c3df97ed8717e377226e8
3omargaad <3omargaad@gmail.com>
2026-06-19 05:01:57 +0100

    Rebased

--- src/gui/python_gui/setup_screen.py
+++ src/gui/python_gui/setup_screen.py
@@ -11,7 +11,7 @@ import audio
 
 class SetupScreen(MDScreen, Screen):
     # rye_font = assets.RYE
-    # roboto_font = assets.ROBOTO
+    roboto_font = assets.ROBOTO
     plr_num = StringProperty("4")
     maf_num = StringProperty("1")
     max_maf = StringProperty("1")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+from kivy.config import Config
+from kivy.resources import resource_add_path
 import app
 
 if __name__ == "__main__":
+    Config.write()
+    resource_add_path("./")
+
     app.run()
 
 # Runs main application
